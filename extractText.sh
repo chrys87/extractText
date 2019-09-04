7a30819885421b351525b816fbab1d8a01fc2dc4
Storm Dragon <stormdragon2976@gmail.com>
2019-09-03 21:11:06 -0400

    some changes and bug fixes.

--- extractText.sh
+++ extractText.sh
@@ -1,81 +1,101 @@
 #!/bin/bash
 
+# Settings to improve accessibility of dialog.
+export DIALOGOPTS='--insecure --no-lines --visit-items'
+
+msgbox() {
+# Returns: None
+# Shows the provided message on the screen with an ok button.
+dialog --clear --msgbox "$*" 10 72
+}
+
+infobox() {
+    # Returns: None
+    # Shows the provided message on the screen with no buttons.
+   local timeout=3 
+    dialog --infobox "$*" 0 0
+    read -n1 -t $timeout continue
+    # Clear any keypresses from the buffer
+    read -t 0.01 continue
+}
+
+yesno() {
+    # Returns: Yes or No
+    # Args: Question to user.
+    # Called  in if $(yesno) == "Yes"
+    # Or variable=$(yesno)
+    dialog --clear --backtitle "$(gettext "Press 'Enter' for \"yes\" or 'Escape' for \"no\".")" --yesno "$*" 10 80 --stdout
+    if [[ $? -eq 0 ]]; then
+        echo "Yes"
+    else
+        echo "No"
+    fi
+}
+
 # variables
-WorkSpacePath=$(mktemp -d)/
-currentDir=$(pwd)
-filename=$(basename -- "$1")
+WorkSpacePath="$(mktemp -d)/"
+currentDir="$(pwd)"
+filename="${1##*/}"
 maxsize=5
 
 # dont continue on error
 set -e
 
 checkRequirements() {
-    # tesseract
-    if ! reqiredCommand="$(type -p "tesseract")" || [[ -z $reqiredCommand ]];
-    then
-        echo 'Error: tesseract is not installed.' >&2
-        exit 1
-    fi
-    # pdftotext
-    if ! reqiredCommand="$(type -p "pdftotext")" || [[ -z $reqiredCommand ]];
-    then
-        echo 'Error: pdftotext is not installed.' >&2
-        exit 1
-    fi
-    if ! reqiredCommand="$(type -p "convert")" || [[ -z $reqiredCommand ]];
-    then
-        echo 'Error: ImageMagick (convert) is not installed.' >&2
-        exit 1
-    fi
-    if ! reqiredCommand="$(type -p "awk")" || [[ -z $reqiredCommand ]];
-    then
-        echo 'Error: awk is not installed.' >&2
-        exit 1
-    fi
+    # Add requirements to the array
+    local requirements=(
+        tesseract
+        pdftotext
+        convert
+        pdftotext
+        awk
+    )
+    for i in "${requirements[@]}" ; do
+        if ! command -v "$i" &> /dev/null ; then
+            infobox "$(gettext "You are missing the required package:") $i"
+            exit 1
+        fi
+    done
 }
 
 initWorkSpace () {
     echo "Prepare workspace $WorkSpacePath"
-    cleanupWorkSpace
-    mkdir $WorkSpacePath
-    cp $1 $WorkSpacePath
+    cp "$1" "$WorkSpacePath/"
 }
 clearWorkSpace() {
-    rm -r /$WorkSpacePath*
-    cp $1 $WorkSpacePath
+    rm "${WorkSpacePath:?}/*" 2> /dev/null
+    cp "$1" "$WorkSpacePath"
 }
+
 cleanupWorkSpace () {
     if [ -d "$WorkSpacePath" ];
     then
-        rm -r $WorkSpacePath
+        rm -r "${WorkSpacePath:?}"
     fi
 }
 
 cleanupFile() {
-    local ret=$(awk '!NF {if (++n <= 2) print; next}; {n=0;print}' $1)
-    echo $ret
+    local ret="$(awk '!NF {if (++n <= 2) print; next}; {n=0;print}' $1)"
+    echo "$ret"
 }
 
 extractPDFFile () {
-    echo "Extract Processing"
-    clearWorkSpace $1
-    local content=$(pdftotext -layout "$WorkSpacePath$filename" "$WorkSpacePath"complete.txt)
+    (clearWorkSpace "$1";
+    local content="$(pdftotext -layout "$WorkSpacePath/$filename" "${WorkSpacePath}/complete.txt")";) \
+        | dialog --clear --programbox "$(gettext "Text extraction in progress...")" 0 0
 }
 
 OCRFile () {
-    echo "OCR Processing"
-    clearWorkSpace $1
-    echo "Split multipage file"
-    convert -density 600 "$WorkSpacePath$filename" "$WorkSpacePath"Seite.png
-    rm "$WorkSpacePath$filename"
-    echo "OCR running"
-    for i in `ls -v $WorkSpacePath | grep .png`;
+    (clearWorkSpace "$1";
+    convert -density 600 "$WorkSpacePath$filename" "${WorkSpacePath}/Seite.png";
+    rm "${WorkSpacePath}/${filename}";
+    for i in "$(ls -v "$WorkSpacePath" | grep .png)" ;
     do
         echo "OCRed $i"
         tesseract "$WorkSpacePath$i" "$WorkSpacePath"output -l deu+eng --oem 0 &> /dev/null
         cat "$WorkSpacePath"output.txt >> "$WorkSpacePath"complete.txt
         rm "$WorkSpacePath"output.txt
-    done
+    done) | dialog --clear --programbox "$(gettext "Text extraction in progress...")" 0 0
 }
 
 proceed () {
@@ -101,4 +121,11 @@ proceed () {
     echo "File saved: $filename.txt"
 }
 
+# Setup gettext
+export TEXTDOMAIN=extractText.sh
+export TEXTDOMAINDIR=/usr/share/locale
+. gettext.sh
+
 proceed $1
+
+exit 0
